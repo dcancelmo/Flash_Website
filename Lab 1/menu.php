451d1934a53a6be3a8233f1e1a61997f7d9a1dbd
sharfuz95 <sshifat@u.rochester.edu>
2018-09-17 15:37:52 -0400

    sharfuz website

---
+++ Lab 1/menu.php
@@ -0,0 +1,6 @@
+<?php
+echo '<li><a href="index.html" class="current"> About</a> </li>
+<li><a href="job.html" > My Jobs</a> </li>
+<li><a href="courses.html" > Current Courses</a> </li>
+<li><a href="involvement.html"> My Involvement</a> </li>'
+?>
