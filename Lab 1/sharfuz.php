729ea3bf1f731e07b0bda346076b1cbce0b82ad8
kchitap <43220337+kchitap@users.noreply.github.com>
2018-09-17 15:48:39 -0400

    add

---
+++ Lab 1/sharfuz.php
@@ -0,0 +1,60 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sharfuz</title>
+  </head>
+
+  <body>
+    <header>
+        
+    </header>
+
+    <h1>Sharfuz Z Shifat</h1>
+    
+    <article>
+      <h2>About Me</h2>
+      <figure>    
+        <img src="./images/sharfuz_sifat.jpg" alt="img1" >
+      </figure>
+      <p>
+      I am an international student from Bangladesh studying at the University of Rochester, NY. 
+      I am double majoring in Computer Science and Economics.
+       My home town is the capital of Bangladesh called Dhaka. 
+      </p>
+    </article>
+
+    <article>
+      <h2 >My Meliora Life</h2>
+
+      <div class="logo"> 
+        <img src="./images/mlp.png" alt="img2" >
+       
+        <img src="./images/RHA.png" alt="img2" >
+
+        <img src="./images/ooo.jpg" alt="img2" >
+           
+      </div>
+            
+      <p>
+        As a freshman I was an EcoRep. I loved that position as it allowed me to learn a lot about sustainable living style and also aware my peer mates about recycling and reducing their carbon foot print.I have always been highly interested in Entrepreneurship. 
+        When I recognized that there wasn’t a proper entrepreneurial ecosystem at UR, I decided to make one.
+        With my friend I co-founded Meliora LaunchPad, which is the undergraduate entrepreneurship student organization at University of Rochester. I am currently the President of the organization. My two other notable involvement includes being the co-founder of Residence Hall Association (RHA) and also being the current president of Order of Omega. 
+      </p>
+    </article>
+
+    <article>
+      <h2>My Jobs</h2>
+      <p> I had my first industry experience last summer, where I worked as a software developer for FedEx Services at Pittsburgh, PA. I worked full stack software development where I was able to learn Angular, React, and Java as back end. 
+      </p> 
+    </article>
+
+    <article>
+      <h2>My Courses</h2>
+      <p> The courses I am taking this semester includes CSC 174 Front End Web Development, CSC 294 : Future User Interface, CSC 244: Knowledge, Representation in AI, CSC 240: Data Mining .
+      </p>
+    </article>  
+    
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<? php include 'inc/top.inc';?>
+<? php include 'inc/main-top.inc';?>
 
 	<body>
+		<h1>Three Pages</h1>
 		<? php include 'inc/nav.inc';?>
 	</body>
 
