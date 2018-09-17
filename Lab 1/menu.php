11b03c508b684c860a5c844a57e558f1915d739b
kchitap <43220337+kchitap@users.noreply.github.com>
2018-09-17 15:52:36 -0400

    done

--- Lab 1/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<li><a href="index.html" class="current"> About</a> </li>
-<li><a href="job.html" > My Jobs</a> </li>
-<li><a href="courses.html" > Current Courses</a> </li>
-<li><a href="involvement.html"> My Involvement</a> </li>'
-?>
