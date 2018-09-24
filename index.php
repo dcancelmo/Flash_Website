f2c5a500a05afebc9d82e5f578e99ccbcdd661c9
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-09-24 16:10:44 -0400

    Head included out and formatting

--- index.php
+++ index.php
@@ -1,26 +1,14 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
-	<head>
-		<meta charset="UTF-8">
-		<title>Flash Website</title>
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-		<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-
-	</head>
-
+	<?php include "inc/head";?>
 	<body>
-	<?php include "inc/header.inc";?>
+		<?php include "inc/header.inc";?>
 		<?php include "inc/menu.inc";?>
-		
-
 		<article>
 			<h2>Our Website</h2>
 			<p>This mixes all of the creator's home pages</p>
 		</article>
 	
-		?php include "inc/scripts.inc"; ?>
+		<?php include "inc/scripts.inc"; ?>
 	</body>
-
 </html>
--- kaleb.php
+++ kaleb.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Flash Website</title>
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-		<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-
-	</head>
+	<?php include "inc/head";?>
 	<body>
 		<?php include "inc/header.inc";?>
 		<?php include "inc/menu.inc";?>
