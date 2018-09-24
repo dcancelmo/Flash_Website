c9874613b9ffdeb25a20de7f6b8fe065628987ea
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-09-24 16:37:42 -0400

    Semantic corrections and whitespace

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
 	<body>
 		<?php include "inc/menu.inc";?>
 		<article>
-			<h2>Our Website</h2>
-			<p>This mixes all of the creator's home pages</p>
+			<h1>Our Website</h1>
+			<div>This mixes all of the creator's home pages.</div>
 		</article>
-	
 		<?php include "inc/scripts.inc"; ?>
 	</body>
 </html>
