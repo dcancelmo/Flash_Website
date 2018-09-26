bed967337a1b0d06161922d8f5422a8927d61591
Efua <kagyarek@u.rochester.edu>
2018-09-26 14:57:26 -0400

    taxonomy

--- kaleb.php
+++ kaleb.php
@@ -12,19 +12,19 @@
 
 		<article>
 			<h2>About Me</h2>
-			<p>I am from Rochester New York and attended high school in the city of Rochester. I have lived within 30 minutes of the city of Rochester for my entire life. Growing up I did martial arts, played golf, soccer, ultimate frisbee, wrestling, baseball, and was involved in music. As a kid I learned how to play piano, trumpet, guitar, and sing, and still do most of these things. I was also involved in musicals throughout middle and high school. Currently at the University I am a member of the Sigma Alpha Mu fraternity, and outside of that spend the rest of my time completing course work.</p>
+			<p>I am from Rochester, New York and attended high school in the city of Rochester. I have lived within 30 minutes of the city of Rochester for my entire life. Growing up I did martial arts, played golf, soccer, ultimate frisbee, wrestling, baseball, and was involved in music. As a kid I learned how to play piano, trumpet, guitar, and sing, and still do most of these things. I was also involved in musicals throughout middle and high school. Currently at the University I am a member of the Sigma Alpha Mu fraternity, and outside of that spend the rest of my time completing course work.</p>
 
-			<h2>Academics</h2>
+			<h2>College Life</h2>
 			<p>I am a Political Science major, with a minor in CSC and a cluster in visual arts. My Political Science major has a focus on theoretical political science and the use of models in research. Using this discipline I plan on completing research in the field of voting institutions, outcomes, and logical choice in voting. After graduating I would like to pursue a PHD in Political Science and do research. Following this I would like to work in government and public service in some way, hopefully in Rochester, my hometown. For this, I have taken: Game Theory, International Relations, Positive Political Theory, Political Economy, Comparative Political Parties, and several other courses. For my art cluster, I have taken courses in studio practice and printmaking.</p>
 
 			<h2>Jobs</h2>
-			<p>The most noteworthy job I’ve had was as a canoe tripping guide for Camp Pathfinder, located in Algonquin Park Ontario. Camp Pathfinder acts as a host for kids from ages 7 to 15 looking to go on wilderness excursions. I spent the majority of my time out on canoe trips, however when needed I would act as a counselor. For my job as a guide, I was in charge of organizing, planning, and leading canoe trips for anywhere from two to fifteen days. A canoe trip consists of reaching different lakes each day by paddling and carrying the canoes. We pack all of our food and clothing into communal packs for 9 people at a time. The only thing we do not bring is firewood, which is collected at each campsite. All cooking is done over an open fire, regardless of weather or location. For this job I earned certifications such as: WFR (Wilderness First Responder), ORCKA (White Water Canoeing Certification), and Bronze Cross wilderness water safety.</p>
+			<p>The most noteworthy job I’ve had was as a canoe tripping guide for Camp Pathfinder, located in Algonquin Park Ontario. Camp Pathfinder acts as a host for kids from ages 7 to 15 looking to go on wilderness excursions. For my job as a guide, I was in charge of organizing, planning, and leading canoe trips for anywhere from two to fifteen days. A canoe trip consists of reaching different lakes each day by paddling and carrying the canoes. We pack all of our food and clothing into communal packs for 9 people at a time. The only thing we do not bring is firewood, which is collected at each campsite. All cooking is done over an open fire, regardless of weather or location. For this job I earned certifications such as: WFR (Wilderness First Responder), ORCKA (White Water Canoeing Certification), and Bronze Cross wilderness water safety.</p>
 
-			<h2>Other Facts</h2>
-			<p>An interesting part of my origin is my heritage on my father’s side specifically. My father was born in Laos, which is a small country in South East Asia which is not represented much in the United States. The country is very small, and because of this most people in the United States have not heard of the country or its rich history. I have visited twice, however I would like to take more trips back and learn the language and the culture first hand. The country is developing quickly, and I am interested in its future.</p>
 		</article>
 
-		<a href="http://www.csc174.org/assignment01/kchitap/index.html">Original Page</a>
+		<footer>
+		<a href="http://www.csc174.org/assignment01/kchitap/index.html">Original Page</a> </footer>
+
 		<?php include "inc/scripts.inc"; ?>
 	</body>
 </html>
--- sharfuz.php
+++ sharfuz.php
@@ -5,28 +5,27 @@
 		<?php include "inc/menu.inc";?>
 
 		<h1>Sharfuz Z Shifat</h1>
-		
+		<figure>
+			<img src="images/sharfuz_sifat.jpg" alt="Sharfuz Z Shifat" >
+			<figcaption>Sharfuz at the University of Rochester</figcaption>
+		</figure>
+
 		<article>
 			<h2>About Me</h2>
-			<figure>
-				<img src="images/sharfuz_sifat.jpg" alt="Sharfuz Z Shifat" >
-				<figcaption>Sharfuz at the University of Rochester</figcaption>
-			</figure>
 
-			<p>I am an international student from Bangladesh studying at the University of Rochester, NY. 
-			I am double majoring in Computer Science and Economics. My home town is the capital of Bangladesh called Dhaka.</p>
+			<p>I am an international student from Bangladesh studying at the University of Rochester, NY. I am double majoring in Computer Science and Economics. My home town is the capital of Bangladesh called Dhaka.</p>
 
-			<h2>My Meliora Life</h2>
+			<h2>College Life</h2>
 			<p> As a freshman I was an EcoRep. I loved that position as it allowed me to learn a lot about sustainable living style and also aware my peer mates about recycling and reducing their carbon foot print.I have always been highly interested in Entrepreneurship. When I recognized that there wasn’t a proper entrepreneurial ecosystem at UR, I decided to make one. With my friend I co-founded Meliora LaunchPad, which is the undergraduate entrepreneurship student organization at University of Rochester. I am currently the President of the organization. My two other notable involvement includes being the co-founder of Residence Hall Association (RHA) and also being the current president of Order of Omega.</p>
 
 			<h2>My Jobs</h2>
 			<p>I had my first industry experience last summer, where I worked as a software developer for FedEx Services at Pittsburgh, PA. I worked full stack software development where I was able to learn Angular, React, and Java as back end.</p> 
-
-			<h2>My Courses</h2>
-			<p> The courses I am taking this semester includes CSC 174 Front End Web Development, CSC 294 : Future User Interface, CSC 244: Knowledge, Representation in AI, CSC 240: Data Mining.</p>
 		</article>
 
+		<footer>
 		<a href= "http://csc174.org/assignment01/sshifat/Assignment01/index.html">Original page</a>
+		</footer>
+
 		<?php include "inc/scripts.inc"; ?>
 	</body>
 </html>
