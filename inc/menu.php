8089eb9e628d60db04f39f37defba166b4b544c9
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-09-26 16:13:05 -0400

    Filetypes

--- inc/menu.php
+++ inc/menu.php
@@ -1,11 +1,8 @@
 <nav>
 	<ul>
 		<li><a href="index.php">Home</a></li>
-
 		<li><a href="kaleb.php">Kaleb</a></li>
-
 		<li><a href="reeshad.php">Reeshad</a></li>
-
 		<li><a href="sharfuz.php">Sharfuz</a></li>
 	</ul>
 </nav>
