20fbf8d931782369a0d422894056dfbc7fe505c8
Efua <kagyarek@u.rochester.edu>
2018-09-26 15:57:34 -0400

    changed p to h2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			<h1>Our Website</h1>
 			<div>Meet Kaleb, Sharfuz and Reeshad: the minds behind the San Fran Team!</div>
 			<footer>
-				<p>Sources: </p>
+				<h2>Sources: </h2>
 				<a href="http://www.csc174.org/assignment01/kchitap/index.html">Kaleb Chitaphong</a>
 				<a href= "http://csc174.org/assignment01/sshifat/Assignment01/index.html">Sharfuz Z Shifat</a>
 				<a href="http://csc174.org/assignment01/rrahman/assignment01/start.html">Reeshad Rahman</a>
